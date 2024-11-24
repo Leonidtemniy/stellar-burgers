18001bdc4e04f3991c405a6a303c129ef1720970
Leonid Temniy <leonidtemniy@yandex.ru>
2024-11-24 23:14:25 +0300

    Начат роутинг

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,31 @@
-import { ConstructorPage } from '@pages';
+import { ConstructorPage } from '../../pages/constructor-page';
+import { Feed } from '../../pages/feed';
+import { Login } from '../../pages/login';
+import { Register } from '@pages';
+import { ForgotPassword } from '@pages';
+import { ResetPassword } from '@pages';
+import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader } from '@components';
 
 const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
+  <BrowserRouter>
+    <div className={styles.app}>
+      <AppHeader />
+      <Routes>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route path='/feed' element={<Feed />} />
+      </Routes>
+    </div>
+  </BrowserRouter>
 );
-
 export default App;
